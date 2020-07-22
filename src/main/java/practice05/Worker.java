5ecd35745a068d45d6abd1ecd2a1c528e1b122eb
Jim-S-Chang <jim.s.chang@qq.com>
2020-07-22 20:09:04 +0800

    practice05 [jim]bug-fix:fix bugs in practice05.

--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -5,6 +5,6 @@ public class Worker extends Person {
         super(name, age);
     }
     public String introduce() {
-        return super.introduce() + "I am a Worker. I have a job.";
+        return super.introduce() + " I am a Worker. I have a job.";
     }
 }
