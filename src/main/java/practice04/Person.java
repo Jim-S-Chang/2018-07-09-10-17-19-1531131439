84312a8354d59a2b5a08656f461afbad4b0ddb6a
Jim-S-Chang <jim.s.chang@qq.com>
2020-07-22 17:12:27 +0800

    practice04-06 [jim]feature:finish coding

--- src/main/java/practice04/Person.java
+++ src/main/java/practice04/Person.java
@@ -1,4 +1,25 @@
 package practice04;
 
 public class Person {
+    private String name;
+    private int age;
+    protected String basicIntroduce;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.basicIntroduce = introduce();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return "My name is " + this.name + ". I am " + this.age + " years old.";
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,25 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int klass = -1;
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if (this.klass != -1) {
+            return super.introduce() + "I am a Teacher. I teach Class " + this.klass + ".";
+        } else {
+            return super.introduce() + "I am a Teacher. I teach No Class.";
+        }
+    }
 }
